aed51199c371fce336dc1e36d8d42624c6c41468
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-01-29 08:57:29 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,21 @@
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        
+       
+        
+       
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
